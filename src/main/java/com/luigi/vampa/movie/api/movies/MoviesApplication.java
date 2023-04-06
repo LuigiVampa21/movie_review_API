83debb2313b298a854c4882884b9bcf55cf24bbb
Nathan Teissier <nmteissier@gmail.com>
2023-04-06 15:00:31 +0200

    springBoot backend

---
+++ src/main/java/com/luigi/vampa/movie/api/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package com.luigi.vampa.movie.api.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luigi/vampa/movie/api/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigi.vampa.movie.api.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
